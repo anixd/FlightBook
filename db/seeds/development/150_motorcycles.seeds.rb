89bd3e7d281dfc7d2423495bfafc10a92b9f8c22
Andrey Gusev <andrey@anix.name>
2024-05-09 13:09:55 +0300

    modified .gitignore

--- db/seeds/development/150_motorcycles.seeds.rb
+++
@@ -1,3 +0,0 @@
-Motorcycle.create(moto_model_id: 1, moto_brand_id: 2, year: 2004, color: 'Серый', vin: 'JYARJ074000005232', plate_number: 'АА4995АА', name: 'Райдо', current_owner_id: 1, visible_to_users: [2])
-Motorcycle.create(moto_model_id: 2, moto_brand_id: 2, year: 2004, color: 'Серый', vin: 'JYARJ074000015982', plate_number: 'АА4995АА', name: 'Фазер', current_owner_id: 1, visible_to_users: [2])
-Motorcycle.create(moto_model_id: 4, moto_brand_id: 1, year: 2004, color: 'Синий', vin: 'JH2RC46C42M402999', plate_number: '', name: 'Фыр', current_owner_id: 1, visible_to_users: [2,3])
